28c9c0485fcc152b446eac98dca390b50e0df46e
Abdullah al rafi <abdullah116632@gmail.com>
2024-11-09 21:27:13 +0600

    setup payment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+"use client";
+
+import CheckoutPage from "@/components/CheckoutPage";
+import convertToSubcurrency from "@/lib/convertToSubcurrency";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+if(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined){
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined")
+}
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
 
 export default function Home() {
+  const amount = 49.99
   return (
-    <main className="">
-      
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="mb-10">
+      <h1 className="text-4xl font-extralight mb-2">Abdullah</h1>
+      <h2 className="text-2xl">
+        has requested
+        <span className="font-bold">${amount}</span>
+      </h2>
+      </div>
+
+      <Elements stripe={stripePromise} options={{
+        mode: "payment",
+        amount: convertToSubcurrency(amount),
+        currency: "usd"
+      }}>
+        <CheckoutPage amount={amount} />
+      </Elements>
     </main>
   );
 }
